10fe4b5a38207e2fda8a3dac5046dd4f682551d6
ciaranlong7 <ciaranlong7@gmail.com>
2023-11-12 14:05:10 +0000

    Applying R_g 'units'

--- Computing_Project.py
+++ Computing_Project.py
@@ -13,19 +13,15 @@ R_g = (G*M)/(c**2)
 h = 6.62607015e-34
 k = 1.380649e-23
 
-#Creating list of R values between 6R_g and 10**5R_g
-r_in = 6*R_g
-r_out = (10**5)*R_g
-
-def T(R):
-    return ((G*M*M_dot)/(8*np.pi*(R**3)*S_B_constant))**(1/4)
+def T(r):
+    return ((G*M*M_dot)/(8*np.pi*((r*R_g)**3)*S_B_constant))**(1/4)
 
 #Taking viscous forces into account
 #Used to throw up warning - RuntimeWarning: invalid value encountered in double_scalars
 #Reason for warning - When I define Rs, the first R value is slightly smaller than the original r_in value
 #This leads to r_in/R > 1 and hence fn is negative and (fn)**(1/4) gives an 'invalid value'
-def T_visc(R):
-    return (((3*G*M*M_dot)/(8*np.pi*(R**3)*S_B_constant))*(1-((r_in/R)**(1/2))))**(1/4)
+def T_visc(r, r_in):
+    return (((3*G*M*M_dot)/(8*np.pi*((r**3)*(R_g**3))*S_B_constant))*(1-((r_in/(r))**(1/2))))**(1/4)
 
 #Defining luminosity per unit frequency per unit area
 #Throwing up warning - RuntimeWarning: overflow encountered in exp
@@ -36,10 +32,10 @@ def F_v(T,v):
 
 #Now need to calculate integral. To do this, I must first calculate the integrand of eq 8.3
 #I can then evaluate the integral using the trapezium rule.
-def integrand(R, v):
-    t = T_visc(R) #Change to T(R) to ignore viscous forces
-    f_v = F_v(t,v)
-    return f_v*4*np.pi*R
+def integrand(r, r_in, v):
+    t = T_visc(r, r_in) #Change to T(R) to ignore viscous forces
+    f_v = F_v(t, v)
+    return f_v*4*np.pi*r*(R_g**2)
 
 #Now evaluate integral with trapezium rule:
 def L_v(r_in, r_out, v, bins):
@@ -53,7 +49,7 @@ def L_v(r_in, r_out, v, bins):
         # Integrand calculated at the midpoint of each bin because at r_in, T_visc=0 giving a div0 error
         midpoint_r = (Rs[i+1] + Rs[i])/2
         new_Rs.append(midpoint_r)
-        my_integrand = integrand(midpoint_r, v)
+        my_integrand = integrand(midpoint_r, 6, v)
         my_integrands.append(my_integrand)
         
     #total sum of all trapeziums
@@ -73,14 +69,14 @@ def spectrum(r_in, r_out):
     
     for v in vs:
         log_vs.append(np.log10(v))
-        l_v, my_integrands = L_v(r_in, r_out, v, 1000)
+        l_v, my_integrands = L_v(6, 10**5, v, 1000) # R_g units
         spectrum.append(l_v)
         log_vl_v.append(np.log10(v*l_v))
     
     return spectrum, vs, log_vs, log_vl_v
 
 #spectrum is luminosity spectrum (luminosity at all frequencies in 10^14 - 10^19 range)
-spectrum, vs, log_vs, log_vl_v = spectrum(r_in, r_out)
+spectrum, vs, log_vs, log_vl_v = spectrum(6, 10**5)
 
 #total luminosity from the system
 #Should be roughly 7x10^30. I get about 7x10^31.
@@ -103,6 +99,8 @@ plt.show()
 #Plotting the difference between T and T_visc
 
 bins = 1000
+r_in = 6
+r_out = 10**5
 Rs = np.logspace(np.log10(r_in), np.log10(r_out), bins)
 new_Rs = []
 Ts = []
@@ -111,7 +109,7 @@ for i in range(len(Rs)-1):
     midpoint_r = (Rs[i+1] + Rs[i])/2
     new_Rs.append(midpoint_r)
     Ts.append(T(midpoint_r))
-    Ts_visc.append(T_visc(midpoint_r))
+    Ts_visc.append(T_visc(midpoint_r, 6))
     
 plt.plot(np.log10(new_Rs), Ts, label = 'no viscous forces considered')
 plt.plot(np.log10(new_Rs), Ts_visc, label = 'viscous forces considered')
@@ -120,6 +118,9 @@ plt.ylabel('T(R) / K')
 plt.title('Temperature as a fn of R (log10 x-axis scale)')
 plt.legend(loc = 'upper right')
 plt.show()
+
+print("Max temperature with viscous forces considered:")
+print(f"{max(Ts_visc):.6e} K")
 
 #Now convergence testing
 def convergence_check():
